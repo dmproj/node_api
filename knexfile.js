581e74d8cf42d9e65aa265ffb8a4665c2a3c8d24
SY <seyav29@gmail.com>
2023-04-21 14:39:29 -0400

    .

--- knexfile.js
+++ knexfile.js
@@ -11,10 +11,8 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     },
-    // needed when using foreign keys
     pool: {
       afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
         conn.run('PRAGMA foreign_keys = ON', done) // turn on FK enforcement
       },
     },
